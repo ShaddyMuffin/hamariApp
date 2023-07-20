4cddd7842814a0d619cd2df45aa32336d6079ca7
Admin <shekha.vishal1@gmail.cmo>
2023-07-21 00:11:49 +0530

    updated simile button and layout

--- app/src/main/java/com/example/hamariapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/hamariapp/ui/dashboard/DashboardFragment.kt
@@ -1,42 +1,45 @@
 package com.example.hamariapp.ui.dashboard
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.hamariapp.databinding.FragmentDashboardBinding
+import com.example.hamariapp.R
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        showDialog()
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+
+        return rootView
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun showDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+
+        builder.setMessage("Do you want to proceed with this action?")
+            .setPositiveButton("Yes") { _: DialogInterface, _: Int ->
+                // Code to execute when "Yes" button is clicked
+                // Add your logic here
+            }
+            .setNegativeButton("No") { dialog: DialogInterface, _: Int ->
+                // Code to execute when "No" button is clicked
+                // Add your logic here
+                dialog.dismiss()
+            }
+            .setCancelable(false)
+
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
     }
+
 }
